a029e0e99f70b2ac7e6780f2d3f03d112909e326
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-07-29 22:55:28 +0500

    completed jwt poject

--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,27 @@
+const { BadRequest } = require("../errors");
+const jwt = require("jsonwebtoken");
+
 const login = async (req, res) => {
-  res.send("Fake Login/SignUp/ route");
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new BadRequest("Please provide email and password");
+  }
+
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "User Created", token });
 };
 
-const dashBoard = async (req, res) => {
+const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
-    msg: "Hello talha",
+    msg: `Hello ${req.user.username}`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
 };
 
-module.exports = { login, dashBoard };
+module.exports = { login, dashboard };
--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const authenticationMiddleWare = require("../middleware/auth");
 
 const { dashboard, login } = require("../controllers/main");
 
-router.route("./dashboard").get(dashboard);
-router.route("./login").post(login);
+router.route("/dashboard").get(authenticationMiddleWare, dashboard);
+router.route("/login").post(login);
 
 module.exports = router;
